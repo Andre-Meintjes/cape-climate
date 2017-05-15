d177478efd226c6387d528ee114894edccba8341
Andre Meintjes <meintjes.paul@gmail.com>
2017-05-15 18:19:04 +0200

    update

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function currentSlide(int){
     var element = document.getElementById("slides");
         
         element.style.backgroundImage = "url('https://raw.githubusercontent.com/Andre-Meintjes/cape-climate/master/" + slides[int] + "')";
-        $('#slides').animateCss('ZoomInUp');
+        $('#slides').animateCss('zoomInUp');
 }
 
 function nextSlide(){
