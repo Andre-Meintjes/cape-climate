0d650fcbf4786cfbec625478e65ab7481457bada
Andre-Meintjes <meintjes.paul@gmail.com>
2017-05-15 15:04:23 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function currentSlide(int){
     var slides = ["/img/maldives.jpg", "/img/artcool.jpg", "/img/emura.jpg"];
     var element = document.getElementById("slides");
         
-        element.style.backgroundImage = "url('..//" + slides[int] + "')";
+        element.style.backgroundImage = "url(\'../" + slides[int] + "\')";
         $('#slides').animateCss('bounceIn');
 }
 
