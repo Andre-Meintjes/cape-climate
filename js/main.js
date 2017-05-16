331a02bb178d03fd46b1eeb751d62a8d0d368f03
Andre Meintjes <meintjes.paul@gmail.com>
2017-05-16 12:43:16 +0200

    btu update

--- js/main.js
+++ js/main.js
@@ -61,10 +61,11 @@ $.fn.extend({
 function calculateBTU(){
     var width = parseInt(document.getElementById("width").Value);
     var length = parseInt(document.getElementById("length").Value);
+    
     var btu= 0;
 
     btu = width * length * 500;
-
+console.log(width + " " + length + " " + btu);
     document.getElementById('btu').innerHTML = "BTUs nedded: " + parseInt(btu);
 
 
